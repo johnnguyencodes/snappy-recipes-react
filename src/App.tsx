afb4408df4f604edc9e5a2987bc3378d298f0895
John Nguyen <jnguyen92780@gmail.com>
2024-10-07 18:33:07 -0700

    cleaned up ui components, added dotenv

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,25 @@
-import React, { Suspense } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { increment } from "./store/counterSlice";
-import { RootState } from "./store/store";
-import { Link, BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Button } from "@/components/ui/button";
-
-const Home = React.lazy(() => import("./pages/Home"));
-const About = React.lazy(() => import("./pages/About"));
+import { Input } from "@/components/ui/input";
+import { useState, ChangeEvent } from "react";
 
 function App() {
-  const dispatch = useDispatch();
-  const count = useSelector((state: RootState) => state.counter);
+  const [query, setQuery] = useState("");
 
-  const message: string = "Hello, Typescript!";
+  const handleQueryChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
   return (
     <>
-      <Router>
-        <h1 className="text-9xl font-bold text-lightmode-blue underline">
-          Hello world! {message}
-        </h1>
-        <Button className="" onClick={() => dispatch(increment())}>
-          count is {count}
-        </Button>
-        <Link className="text-6xl text-black hover:text-gray-200" to="/">
-          Home
-        </Link>
-        <Link className="text-6xl text-black hover:text-gray-200" to="/about">
-          About
-        </Link>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </Suspense>
-      </Router>
+      <h1>Recipe Search</h1>
+      <div>
+        <Input
+          id="input"
+          placeholder="placeholder"
+          onChange={(event) => handleQueryChange(event)}
+        />
+        <Button onClick={() => console.log(query)}>Submit</Button>
+      </div>
     </>
   );
 }
