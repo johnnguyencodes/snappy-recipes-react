94eeacce9e4b8a98cfeb3238571d182f23ad8db9
John Nguyen <jnguyen92780@gmail.com>
2024-11-13 17:34:25 -0800

    reverted fileValidation to use dependenc injection

--- src/lib/formUtils.ts
+++ src/lib/formUtils.ts
@@ -10,37 +10,39 @@ const MAX_FILE_SIZE = 10485760; // 10MB
 
 const fileValidation = (
   event: ChangeEvent<HTMLInputElement>,
-  showError: (errorType: string) => void
-): File | null => {
+  showError: (errorType: string) => void,
+  setImageFile: (file: File) => void
+): boolean => {
   const files = event.target.files;
-  if (!files || files.length === 0) {
-    showError("errorNoFile");
-    return null;
+  if (files && files.length > 0) {
+    const file: File = files[0];
+
+    if (!file) {
+      showError("errorNoFile");
+      return false;
+    }
+
+    // The order of validation checks matters to ensure correct and meaningful error handling.
+    // Checking file size first ensures large files are always rejected before evaluating other conditions.
+    // This prevents misleading errors (e.g., "incorrect file type" for oversized files) and provides a better user experience.
+
+    // Check for file size first
+    if (file.size > MAX_FILE_SIZE) {
+      showError("errorFileExceedsSize");
+      return false;
+    }
+
+    // Check for file type second
+    if (!Object.values(FileType).includes(file.type as FileType)) {
+      showError("errorIncorrectFile");
+      return false;
+    }
+
+    setImageFile(file);
+    return true;
   }
-
-  const file = files[0];
-  if (!file) {
-    showError("errorNoFile");
-    return null;
-  }
-
-  // The order of validation checks matters to ensure correct and meaningful error handling.
-  // Checking file size first ensures large files are always rejected before evaluating other conditions.
-  // This prevents misleading errors (e.g., "incorrect file type" for oversized files) and provides a better user experience.
-
-  // Check for file size first
-  if (file.size > MAX_FILE_SIZE) {
-    showError("errorFileExceedsSize");
-    return null;
-  }
-
-  // Check for file type second
-  if (!Object.values(FileType).includes(file.type as FileType)) {
-    showError("errorIncorrectFile");
-    return null;
-  }
-
-  return file;
+  showError("errorNoFile");
+  return false;
 };
 
 const showError = (errorType: string) => {
