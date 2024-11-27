85f6eaa4edc492503f3897000d2980b732b16e9a
John Nguyen <jnguyen92780@gmail.com>
2024-11-26 21:28:00 -0800

    Updated test

--- src/tests/vitest/showError.test.ts
+++ src/tests/vitest/showError.test.ts
@@ -4,7 +4,7 @@ import { describe, it, expect, vi } from "vitest";
 describe("showError", () => {
   it("should set the correct error message for `errorNoFile`", () => {
     const mockSetErrorMessage = vi.fn();
-    showError("errorNoFile", mockSetErrorMessage);
+    showError("errorNoFile", mockSetErrorMessage, null);
     expect(mockSetErrorMessage).toHaveBeenCalledWith(
       "No file selected. Please select a file."
     );
@@ -12,7 +12,7 @@ describe("showError", () => {
 
   it("should set the corret error message for `errorIncorrectFile`", () => {
     const mockSetErrorMessage = vi.fn();
-    showError("errorIncorrectFile", mockSetErrorMessage);
+    showError("errorIncorrectFile", mockSetErrorMessage, null);
     expect(mockSetErrorMessage).toHaveBeenCalledWith(
       "Invalid file type. Please upload an image file."
     );
@@ -20,7 +20,7 @@ describe("showError", () => {
 
   it("should set the correct error message for `errorFileExceedsSize`", () => {
     const mockSetErrorMessage = vi.fn();
-    showError("errorFileExceedsSize", mockSetErrorMessage);
+    showError("errorFileExceedsSize", mockSetErrorMessage, null);
     expect(mockSetErrorMessage).toHaveBeenCalledWith(
       "File size exceeds the allowed limit."
     );
@@ -28,13 +28,13 @@ describe("showError", () => {
 
   it("should set an empty message for `noError`", () => {
     const mockSetErrorMessage = vi.fn();
-    showError("noError", mockSetErrorMessage);
+    showError("noError", mockSetErrorMessage, null);
     expect(mockSetErrorMessage).toHaveBeenCalledWith("");
   });
 
   it("should set a default error message for unknown error types", () => {
     const mockSetErrorMessage = vi.fn();
-    showError("unknownErrorType", mockSetErrorMessage);
+    showError("unknownErrorType", mockSetErrorMessage, null);
     expect(mockSetErrorMessage).toHaveBeenCalledWith(
       "An unknown error occurred."
     );
