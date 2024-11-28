07c50dc4464fcbddc56e5a081be208438384cb69
John Nguyen <jnguyen92780@gmail.com>
2024-11-27 21:48:56 -0800

    display selected image on page

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,15 @@ import {
 } from "./lib/apiUtils";
 
 function App() {
-  const [query, setQuery] = useState("");
-  const [_imageFile, setImageFile] = useState<File | null>(null);
-  const [imgurAccessToken, setImgurAccessToken] = useState("");
-  const [statusMessage, setStatusMessage] = useState<string | null>(null);
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const fileInputRef = useRef<HTMLInputElement | null>(null);
   const searchInputRef = useRef<HTMLInputElement | null>(null);
+  const [imgurAccessToken, setImgurAccessToken] = useState("");
+  const [previousFile, setPreviousFile] = useState<File | null>(null);
+  const [_imageFile, setImageFile] = useState<File | null>(null);
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+  const [query, setQuery] = useState("");
+  const [statusMessage, setStatusMessage] = useState<string | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchAccessToken = async () => {
@@ -34,6 +36,15 @@ function App() {
     };
     fetchAccessToken();
   }, []);
+
+  useEffect(() => {
+    // Cleanup the preview URL when the component unmounts or the file changes
+    return () => {
+      if (previewUrl) {
+        URL.revokeObjectURL(previewUrl); // Free memory by revoking the URL
+      }
+    };
+  }, [previewUrl]);
 
   const handleUploadButtonClick = () => {
     if (fileInputRef.current) {
@@ -59,94 +70,133 @@ function App() {
     if (searchInputRef.current) {
       searchInputRef.current.value = "";
     }
-    document.getElementById;
-    let selectedFile: File | null = null;
-    // Validate file input
-    const isValid = fileValidation(
-      event,
-      showError,
-      (file) => {
-        setImageFile(file); // Update State
-        selectedFile = file; // Immediate access to file
-      },
-      setErrorMessage,
-      clearErrorMessage
-    );
-    if (isValid && selectedFile) {
-      setStatusMessage("Analyzing image");
-      // Prepare form data for Imgur upload
-      const formData = appendImgurFormData(selectedFile); // Call the utility function to handle form data and image upload
 
-      try {
-        // Call the Imgur API
-        let imgurJson;
-        try {
-          imgurJson = await postImage(
-            formData,
-            imgurAccessToken,
-            showError,
-            setErrorMessage
-          );
-        } catch (error) {
-          console.error("Error uploading image to Imgur:", error);
-          setStatusMessage("");
-          return;
-        }
+    let currentFile: File | null = null;
+    const files = event.target.files;
+    if (files && files.length > 0) {
+      currentFile = files[0];
 
-        const imageURL = imgurJson.data.link;
-
-        // Call Google Vision API
-        let googleJson;
-        try {
-          googleJson = await postImageUrlToGoogle(
-            imageURL,
-            showError,
-            setErrorMessage
-          );
-        } catch (error) {
-          console.error("Error fetching data from Google Vision API:", error);
-          setStatusMessage("");
-          return;
-        }
-
-        const labelAnnotations = googleJson.responses[0]?.labelAnnotations;
-        if (!labelAnnotations || labelAnnotations.length === 0) {
-          showError("errorNoLabelAnnotations", setErrorMessage, null);
-          throw new Error("No label annotations found in Google API response");
-        }
-
-        const [firstAnnotation] = labelAnnotations;
-        // @ts-ignore
-        const { description: imageTitle, score } = firstAnnotation;
-        setQuery(imageTitle);
-
-        // Call Spoonacular API
-        try {
-          setStatusMessage(`Searching for recipes with ${imageTitle}`);
-          const spoonacularJson = await getRecipes(
-            imageTitle,
-            showError,
-            setErrorMessage
-          );
-          console.log("Spoonacular API response:", spoonacularJson);
-          setStatusMessage("");
-        } catch (error) {
-          setStatusMessage("");
-          console.error("Error fetching data from Spoonacular API:", error);
-        }
-      } catch (error) {
-        setStatusMessage("");
-        console.error("Unexpected error during API calls:", error);
+      // Check if the same file is selected
+      if (
+        previousFile &&
+        currentFile &&
+        currentFile.name === previousFile.name &&
+        currentFile.size === previousFile.size &&
+        currentFile.lastModified === previousFile.lastModified
+      ) {
+        console.warn("The same image file was selected again.");
+        showError("errorSameImage", setErrorMessage, null);
+        return;
       }
-    } else {
-      setStatusMessage("");
-      console.error("No valid file selected");
+
+      // Update the previously selected file
+      setPreviousFile(currentFile);
+
+      let selectedFile: File | null = null;
+      // Validate file input
+      const isValid = fileValidation(
+        event,
+        showError,
+        (currentFile) => {
+          setImageFile(currentFile); // Update State
+          setPreviewUrl(URL.createObjectURL(currentFile)); // Create a temporary URL for preview
+          selectedFile = currentFile; // Immediate access to file
+        },
+        setErrorMessage,
+        clearErrorMessage
+      );
+      if (isValid && selectedFile) {
+        setStatusMessage("Analyzing image");
+        // Prepare form data for Imgur upload
+        const formData = appendImgurFormData(selectedFile); // Call the utility function to handle form data and image upload
+
+        try {
+          // Call the Imgur API
+          let imgurJson;
+          try {
+            imgurJson = await postImage(
+              formData,
+              imgurAccessToken,
+              showError,
+              setErrorMessage
+            );
+          } catch (error) {
+            console.error("Error uploading image to Imgur:", error);
+            setStatusMessage("");
+            return;
+          }
+
+          const imageURL = imgurJson.data.link;
+
+          // Call Google Vision API
+          let googleJson;
+          try {
+            googleJson = await postImageUrlToGoogle(
+              imageURL,
+              showError,
+              setErrorMessage
+            );
+          } catch (error) {
+            console.error("Error fetching data from Google Vision API:", error);
+            setStatusMessage("");
+            return;
+          }
+
+          const labelAnnotations = googleJson.responses[0]?.labelAnnotations;
+          if (!labelAnnotations || labelAnnotations.length === 0) {
+            showError("errorNoLabelAnnotations", setErrorMessage, null);
+            throw new Error(
+              "No label annotations found in Google API response"
+            );
+          }
+
+          const [firstAnnotation] = labelAnnotations;
+          // @ts-ignore
+          const { description: imageTitle, score } = firstAnnotation;
+          setQuery(imageTitle);
+
+          // Call Spoonacular API
+          try {
+            setStatusMessage(`Searching for recipes with ${imageTitle}`);
+            const spoonacularJson = await getRecipes(
+              imageTitle,
+              showError,
+              setErrorMessage
+            );
+            console.log("Spoonacular API response:", spoonacularJson);
+            setStatusMessage("");
+          } catch (error) {
+            setStatusMessage("");
+            console.error("Error fetching data from Spoonacular API:", error);
+          }
+        } catch (error) {
+          setStatusMessage("");
+          console.error("Unexpected error during API calls:", error);
+        }
+      } else {
+        setStatusMessage("");
+        setPreviewUrl(null); // Clear preview if validation fails
+        console.error("No valid file selected");
+      }
+    }
+
+    // Clear the file input's value to allow reselecting the same file
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
     }
   };
 
   const handleSearch = async (query: string) => {
     setImageFile(null);
     setErrorMessage("");
+    if (previewUrl) {
+      URL.revokeObjectURL(previewUrl); // Free memory by revoking the URL
+      setPreviewUrl(null);
+    }
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
+    }
+    setPreviousFile(null);
 
     // Validate search input
     const isValid = searchValidation(
@@ -230,6 +280,16 @@ function App() {
         {errorMessage && (
           <div className="error-message mb-4 rounded bg-red-100 p-2 text-red-600">
             {errorMessage}
+          </div>
+        )}
+        {previewUrl && (
+          <div>
+            <img
+              src={previewUrl}
+              alt="Selected image preview"
+              width="200px"
+              height="100%"
+            />
           </div>
         )}
       </div>
