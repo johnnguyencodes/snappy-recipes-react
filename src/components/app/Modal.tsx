c9cf493ee1b48c4cc038a50eb387fd41374a2d98
John Nguyen <jnguyen92780@gmail.com>
2025-01-05 16:25:48 -0800

    updated card and Settings UI

--- src/components/app/Modal.tsx
+++ src/components/app/Modal.tsx
@@ -43,7 +43,11 @@ const Modal: React.FC<IModalProps> = ({
         className="fixed left-1/2 top-1/2 z-50 max-h-[80%] max-w-lg -translate-x-1/2 -translate-y-1/2 transform overflow-auto rounded-md bg-lightmode-panel p-6 opacity-100 shadow-lg dark:bg-darkmode-dark1"
         id="modal-content"
       >
-        {title && (
+        {!title ? (
+          // If there's no explicit title prop, still provide a hidden one
+
+          <DialogTitle className="sr-only">Hidden Title</DialogTitle>
+        ) : (
           <DialogTitle
             id="modal-title"
             className="mx-auto text-center text-2xl font-bold text-lightmode-red dark:text-darkmode-yellow"
@@ -51,7 +55,12 @@ const Modal: React.FC<IModalProps> = ({
             {title}
           </DialogTitle>
         )}
-        {description && (
+        {!description ? (
+          // Provide a hidden description if not passed in
+          <DialogDescription className="sr-only">
+            Hidden description
+          </DialogDescription>
+        ) : (
           <DialogDescription
             id="modal-description"
             className="mx-auto text-sm text-muted-foreground"
