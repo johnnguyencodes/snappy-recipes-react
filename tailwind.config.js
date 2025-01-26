6c2f02be3728d1d08c88fed2d2651e7393b0e84e
John Nguyen <jnguyen92780@gmail.com>
2025-01-25 18:49:41 -0800

    updated skeleton card styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -140,11 +140,20 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        shimmer: {
+          "0%": { backgroundPosition: "200% 0" },
+          "100%": { backgroundPosition: "-200% 0" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        shimmer: "shimmer 4s linear infinite",
       },
+      backgroundImage: (theme) => ({
+        shimmerLight: `linear-gradient(to left, ${theme("colors.lightmode-dimmed4")} 25%, ${theme("colors.lightmode-dimmed5")} 50%, ${theme("colors.lightmode-dimmed4")} 75%)`,
+        shimmerDark: `linear-gradient(to left, ${theme("colors.darkmode-dimmed4")} 25%, ${theme("colors.darkmode-dimmed5")} 50%, ${theme("colors.darkmode-dimmed4")} 75%)`,
+      }),
       typography: (theme) => ({
         maincolors: {
           css: {
